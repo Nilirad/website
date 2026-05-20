36e8ec5640e76368d3cfa265dff0c5a0bebdbcf1
Nilirad <gisquerin@gmail.com>
2026-05-20 14:53:14 +0200

    Scaffold project

---
+++ README.md
@@ -0,0 +1,27 @@
+# nilirad.dev website
+
+Source code for the [nilirad.dev] website.
+This website uses [Zola],
+and is hosted on [Cloudflare Pages].
+
+<!-- LINKS -->
+[Zola]: https://getzola.org
+[nilirad.dev]: https://nilirad.dev
+[Cloudflare Pages]: https://pages.cloudflare.com/
+
+---
+
+## License
+
+This repository is dual-licensed under the following,
+unless otherwise noted:
+
+- [MIT LICENSE][mit]
+- [Apache License, Version 2.0][apache]
+
+at your option.
+
+<!-- LINKS -->
+
+[mit]: https://opensource.org/license/mit
+[apache]: https://www.apache.org/licenses/LICENSE-2.0
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Development environment for Zola project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: import nixpkgs { inherit system; };
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.zola
+            ];
+          };
+        }
+      );
+    };
+}
